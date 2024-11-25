411b3d45cfec4da8fb9db9e8e7b25793bf9b3a2d
3joker1789 <rv.saritamac@uea.edu.ec>
2024-11-25 00:07:57 -0500

    avance

--- UNIDAD 3/ejercicios.py
+++ UNIDAD 3/ejercicios.py
@@ -26,15 +26,32 @@ def calculator():
         print("5.Salir")
 
         option=int(input("Ingrese su opcion (1,2,3,4,5):"))
-        if option==5
+        if option==5:
             print("Saliendo de la calculadora")
+            break
 
-        if option in [1,2,3,4]:
-            num1=float("ingrese primer numero:")
-            num1=float("ingrese el segundo nuemro:")
-        if option=="1":
-            print("la suma es":,add(num1, num2))
-        elif option=="2":
+        if option in ["1","2","3","4"]:
+            num1= float(input("ingrese primer numero:"))
+            num2= float(input("ingrese el segundo numero:"))
+
+            if option=="1":
+                print("la suma es:",add(num1, num2))
+            elif option=="2":
+                print("La resta es :", substract(num1,num2))
+            elif option=="3":
+                print("La Multiplicacion es:", multiply(num1,num2))
+            elif option=="4":
+                print("La division es:", divide(num1,num2))
+
+        else:
+            print("Opcion no valida, porfavor intenta de nuevo ")
+
+calculator()
+
+
+
+
+
 
 
 
