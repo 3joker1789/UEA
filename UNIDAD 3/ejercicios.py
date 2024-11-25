0957cdae910e34dae2e60031bd747fd96575e81a
3joker1789 <rv.saritamac@uea.edu.ec>
2024-11-24 23:13:49 -0500

    prractica

---
+++ UNIDAD 3/ejercicios.py
@@ -0,0 +1,40 @@
+def greet(name, last_name):
+    print("Hola",name, last_name)
+greet("Ramiro","Saritama")
+greet("Verito",  "Astudillo")
+
+
+def add(a,b):
+    return a+b
+
+def substract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+def calculator():
+    while True:
+        print("Seleccioe una operacion")
+        print("1.Suma")
+        print("2.Resta")
+        print("3. Multipicacion")
+        print("4 Division")
+        print("5.Salir")
+
+        option=int(input("Ingrese su opcion (1,2,3,4,5):"))
+        if option==5
+            print("Saliendo de la calculadora")
+
+        if option in [1,2,3,4]:
+            num1=float("ingrese primer numero:")
+            num1=float("ingrese el segundo nuemro:")
+        if option=="1":
+            print("la suma es":,add(num1, num2))
+        elif option=="2":
+
+
+
